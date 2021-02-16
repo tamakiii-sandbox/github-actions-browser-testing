2850112f0ddaee7d5fae444dccc0f8271e34e72f
tamakiii <tamakiii@users.noreply.github.com>
2021-02-17 01:12:12 +0900

    mv steps to github-actions.mk

--- github-actions.mk
+++ github-actions.mk
@@ -1,4 +1,16 @@
-.PHONY: help
+.PHONY: help install test clean
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
+
+install: \
+	node_modules
+
+test:
+	node src/main.js
+
+node_modules:
+	npm install --include=dev
+
+clean:
+	rm -rf node_modules
