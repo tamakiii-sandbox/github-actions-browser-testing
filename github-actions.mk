8a6ccf2baca2b88babf8dd51b47b35d865d9d88a
tamakiii <tamakiii@users.noreply.github.com>
2021-02-17 02:26:39 +0900

    fix

--- github-actions.mk
+++ github-actions.mk
@@ -7,7 +7,8 @@ install: \
 	node_modules
 
 start:
-	timeout 10 npx --no-install http-server -a 0.0.0.0 -p 3030 test
+	timeout 10 npx --no-install http-server -a 0.0.0.0 -p 3030 test &
+	sleep 3
 
 test:
 	node src/main.js
